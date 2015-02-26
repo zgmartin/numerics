6ed5c521e4a2f16f9747af2a5f2adaa183a79d6b
zgmartin <zacharygilmartin@gmail.com>
2015-02-26 12:34:59 -0500

    integarte update

--- integrate/example.py
+++ integrate/example.py
@@ -1,12 +1,27 @@
 import time
 from integrate import generator
 from matplotlib import pyplot
-from operator import div
-
 
 #uniform random numbers
-x = generator(time.time(), 20000000)
+x = generator(time.time(),  20000000, 10)
 results = pyplot.hist(x,20)
 results = results[0]
-n =  len(results)
-presentages = map()
+
+n = 20000000.0
+percentages = map(lambda x: x/n, results)
+print percentages
+n = len(percentages)
+average = sum(percentages)/n
+print 'percent average:', average
+
+
+
+
+
+#matrix
+
+
+
+
+#integration 
+
--- integrate/integrate.py
+++ integrate/integrate.py
@@ -2,32 +2,68 @@
 A collection of methods for integrating functions.
 
     Methods:
-    Trapezoidal 
-    Monte Carlo  
+        Trapezoidal 
+        Monte Carlo  
 """
+import random
 
-def generator(seed, size):
+def generator(seed, size, interval):
     """
     Generates a list of random numbers.
 
     inputs:
         seed: random seed value
-        amount: 
+        size: the number of elements in the list
 
     """
     
     numbers = []
 
     for n in range(size):
-        seed = random_uniform(seed)
+        seed = random_uniform(seed,interval)
         numbers.append(seed)
 
     return numbers
 
 
-def random_uniform(number):
+def random_uniform(number, interval):
     """
-    A uniformly distributed random number generator on interval [-10,10]. 
+    A uniformly distributed random number on interval [-10,10]. 
     """
 
-    return (7 * number) % 20.0 - 10
+    return (7 * number) % 2.0*interval - interval
+
+
+def trapezoid(function):
+    pass
+
+
+def montecarlo(function):
+    pass
+
+
+
+class Matrix:
+    """
+    A random matrix object. 
+    """
+
+    def __init__(self, rows, columns):
+        self.data = []
+        self.rows = rows
+        self.columns = columns
+
+        for row in range(rows):
+            self.data.append([])
+            for column in range(columns):
+                self.data[row].append(random.uniform(-1,1))
+    def __
+    def __str__(self):
+        result = ''
+
+        for row in range(self.rows):
+            result = result + str(self.data[row])+'\n'
+
+        return result 
+
+
