46214b85398abd60039fc7603d913f261a407780
zgmartin <zacharygilmartin@gmail.com>
2015-04-17 18:42:44 -0400

    roots update

--- roots/example.py
+++ roots/example.py
@@ -1,17 +1,29 @@
 """
 Root Finding Problem:
-Finding roots of specific function. 
+Finding roots of a function. 
 """
 
 from roots import bisection, newton
 from math import sin, cos, exp
+from matplotlib import pyplot
+from numpy import linspace
 
 #test function
 def f(x):
     return pow(2.015, -pow(x,3)) - pow(x,4)*sin(pow(x,3)) - 1.984
 
-#methods performed on function
-print bisection(f,[1.5,1.6],.0001)
-print newton(f,1.6,.0001)
+#results
+bisect = bisection(f,[1.5,1.6],.0001)
+newt = newton(f,1.6,.0001)
+print 'bisection:', bisect
+print 'newton', newt
 
-
+#plots
+x = linspace(0,2)
+y = map(f, x)
+pyplot.scatter(bisect, 0, s=80, facecolors='none', edgecolors='r')
+pyplot.annotate('here it is', xy=(bisect,0), xytext=(1,1), arrowprops=dict(facecolor='black', shrink=0.05))
+pyplot.xlim(0,2)
+pyplot.plot(x,y)
+pyplot.axis('off')
+pyplot.show() 
